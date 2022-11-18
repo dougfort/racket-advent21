5f35d3f039632a88981737177d67fcd3c4e91cf4
dougfort <doug.fort@gmail.com>
2022-11-18 10:07:03 -0500

    Crudely completed day 4 part 2 by displaying all the results

--- advent04.rkt
+++ advent04.rkt
@@ -3,7 +3,16 @@
 (require "advent04-data.rkt" "advent04-board.rkt")
 
 (define (find-winning-board board-data numbers)
-  (let ([boards (map make-board board-data)])
-    (for/or ([number (in-list numbers)])
-      (for/or ([board (in-list boards)])
-        (mark-board board number)))))
+  (let ([boards (map make-board board-data)]
+        [completed-boards (make-hash)])
+    (for ([number (in-list numbers)])
+      (for ([i (in-naturals)]
+            [board (in-list boards)]
+            #:unless (hash-has-key? completed-boards i))
+        (let ([score (mark-board board number)])
+          (if score
+              (begin
+                (printf "board ~s: score ~s\n" i score)
+                (hash-set! completed-boards i #t))
+              #f))))))
+                                    
