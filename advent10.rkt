29e6336cb094e6dfd3f61abcb56ceeaddead9897
dougfort <doug.fort@gmail.com>
2022-11-24 15:54:42 -0500

    Day 10 part 2

--- advent10.rkt
+++ advent10.rkt
@@ -3,14 +3,15 @@
 (require "advent10-data.rkt")
 
 (define chunk-bounds #hash((#\( . #\)) (#\[ . #\])  (#\{ . #\}) (#\< . #\>)))
-(define scores #hash((#\) . 3) (#\] . 57)  (#\} . 1197) (#\> .  25137)))
+(define defect-scores #hash((#\) . 3) (#\] . 57)  (#\} . 1197) (#\> .  25137)))
+(define completion-scores #hash((#\) . 1) (#\] . 2)  (#\} . 3) (#\> .  4)))
 
-; return #f or the first incorrect closing character
+; return a pair of the completion stack with the first incorrect closing character 
 (define (scan l)
   (let loop ([chs (string->list l)]
              [stack '()])
     (cond
-      [(empty? chs) #f]
+      [(empty? chs) (cons stack #f)]
       [else
        (let* ([ch (car chs)]
               [closer (hash-ref chunk-bounds ch #f)])
@@ -19,7 +20,7 @@
            [else
             (if (equal? ch (car stack))
                 (loop (cdr chs) (cdr stack))
-                ch)]))])))
+                (cons stack ch))]))])))
 
 (define (scan-all d)
   (for/fold ([acc null]
@@ -30,9 +31,25 @@
           (cons err acc)
           acc))))
 
-(define (compute-score xs)
+(define (sum-defect-scores xs)
   (for/sum ([x (in-list xs)])
-    (hash-ref scores x 0)))
-      
-(compute-score (scan-all test-data))
-(compute-score (scan-all data))  
+    (hash-ref defect-scores (cdr x) 0)))
+
+(sum-defect-scores (scan-all test-data))
+(sum-defect-scores (scan-all data))
+
+(define (compute-completion-score x)
+  (for/fold ([score 0])
+            ([cl (in-list (car x))])
+    (+ (* score 5) (hash-ref completion-scores cl 0))))
+             
+(define (list-completion-scores xs)
+  (for/list ([x (in-list xs)]
+             #:when (not (cdr x)))
+    (compute-completion-score x)))
+
+(define (select-completion-score cs)
+  (list-ref cs (quotient (length cs) 2)))
+
+(select-completion-score (sort (list-completion-scores (scan-all test-data)) <))
+(select-completion-score (sort (list-completion-scores (scan-all data)) <))
